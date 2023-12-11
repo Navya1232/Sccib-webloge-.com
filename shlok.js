1a4ead18cd77e6b80b7da2526f3601711fb2a689
Navya1232 <144546657+Navya1232@users.noreply.github.com>
2023-12-11 18:37:00 +0530

    these website is for learners

---
+++ shlok.js
@@ -0,0 +1,17 @@
+var time = prompt("Hey! What's The Time: ");
+if (time >5 && time < 12) {
+    alert("Good Morning");
+}
+
+else if (time >12 && time < 18) {
+    alert("Good Afternoon");
+}
+
+else if (time >18 && time < 22 ) {
+    alert("Good Evening");
+}
+
+else {
+    alert("Good Night");
+}
+
